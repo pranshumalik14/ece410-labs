27d3b5bf54f8fb654bd3621779911026c81c77ff
Pranshu Malik <pranshumalik14@gmail.com>
2021-10-03 15:57:09 -0400

    adding figures

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -9,7 +9,7 @@ clear all;
 
 %% numerical simulation
 
-% set up <>
+% set up initial values and DE parameters
 parameters = struct('M', 0.2, 'g', 9.81, 'l', 0.15);
 x_0        = [0                               0; 
               sqrt(parameters.g/parameters.l) 0.199*sqrt(parameters.g/parameters.l)];
@@ -20,47 +20,63 @@ Tspan      = linspace(0,10,1e3);
 [t_1,x_1t] = ode45(@pendulum, Tspan, x_0(:,1), options, parameters);
 [t_2,x_2t] = ode45(@pendulum, Tspan, x_0(:,2), options, parameters);
 
-% plotting (todo: fix labels, aspect ratios, export to files)
+% plotting
 x_1t_1 = x_1t(:, 1); x_1t_2 = x_1t(:, 2);
 x_2t_1 = x_2t(:, 1); x_2t_2 = x_2t(:, 2);
 
-f_1_1 = figure('Name','Individual State Evolution (starting x_0_1)','NumberTitle','off');
-f_1_2 = figure('Name','State Orbit (starting x_0_1)','NumberTitle','off');
+f_1_1   = figure('Name','Individual State Evolution (starting x_0_1)','NumberTitle','off');
+f_1_2_a = figure('Name','State Orbit (starting x_0_1; axis equal)','NumberTitle','off');
+f_1_2_b = figure('Name','State Orbit (starting x_0_1)','NumberTitle','off');
 
 figure(f_1_1);
+
 subplot(2,1,1);
 plot(t_1, x_1t_1);
-xlabel('Time (seconds)');
-ylabel('$\theta$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+
 subplot(212);
 plot(t_1, x_1t_2);
-xlabel('Time (seconds)');
-ylabel('$\dot{\theta}$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
 
-figure(f_1_2);
+figure(f_1_2_a);
+plot(x_1t_2, x_1t_1);
+daspect([1 1 1])
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+xlabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+
+figure(f_1_2_b);
 plot(x_1t_1, x_1t_2);
-% todo: control aspect ratio of this plot
-xlabel('x_1');
-ylabel('x_2');
+xlabel('$\theta$ [rad]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
 
-f_2_1 = figure('Name','Individual State Evolution (starting x_0_2)','NumberTitle','off');
-f_2_2 = figure('Name','State Orbit (starting x_0_2)','NumberTitle','off');
+f_2_1   = figure('Name','Individual State Evolution (starting x_0_2)','NumberTitle','off');
+f_2_2_a = figure('Name','State Orbit (starting x_0_2; axis equal)','NumberTitle','off');
+f_2_2_b = figure('Name','State Orbit (starting x_0_2)','NumberTitle','off');
 
 figure(f_2_1);
+
 subplot(2,1,1);
 plot(t_2, x_2t_1);
-xlabel('Time (seconds)');
-ylabel('$\theta$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+
 subplot(212);
 plot(t_2, x_2t_2);
-xlabel('Time (seconds)');
-ylabel('$\dot{\theta}$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
 
-figure(f_2_2);
+figure(f_2_2_a);
+plot(x_2t_2, x_2t_1);
+daspect([1 1 1])
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+xlabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+
+figure(f_2_2_b);
 plot(x_2t_1, x_2t_2);
-% todo: control aspect ratio of this plot
-xlabel('x_1');
-ylabel('x_2');
+xlabel('$\theta$ [rad]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
 
 %% symbolic linearization
 
@@ -101,59 +117,87 @@ augmented_pend = matlabFunction(Xdot, 'vars', {t, [x;z]});
 [t_1,X_1t] = ode45(augmented_pend, Tspan, [x_0(:,1); x_0(:,1)], options);
 [t_2,X_2t] = ode45(augmented_pend, Tspan, [x_0(:,2); x_0(:,2)], options);
 
-% plotting (todo: fix labels, aspect ratios, export to files)
+% plotting
 x_1t_1 = X_1t(:, 1); x_1t_2 = X_1t(:, 2); z_1t_1 = X_1t(:, 3); z_1t_2 = X_1t(:, 4);
 x_2t_1 = X_2t(:, 1); x_2t_2 = X_2t(:, 2); z_2t_1 = X_2t(:, 3); z_2t_2 = X_2t(:, 4);
 
-F_1_1 = figure('Name','Individual State Evolution (starting X_0_1)','NumberTitle','off');
-F_1_2 = figure('Name','State Orbit (starting X_0_1)','NumberTitle','off');
+F_1_1   = figure('Name','Individual State Evolution (starting X_0_1)','NumberTitle','off');
+F_1_2_a = figure('Name','State Orbit (starting X_0_1; axis equal)','NumberTitle','off');
+F_1_2_b = figure('Name','State Orbit (starting X_0_1)','NumberTitle','off');
 
 figure(F_1_1);
+
 subplot(2,1,1);
 plot(t_1, x_1t_1);
 hold on;
 plot(t_1, z_1t_1);
-xlabel('Time (seconds)');
-ylabel('$\theta$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+set(legend('$\theta_x$', '$\theta_z$'), 'Interpreter', 'latex');
+
 subplot(212);
 plot(t_1, x_1t_2);
 hold on;
 plot(t_1, z_1t_2);
-xlabel('Time (seconds)');
-ylabel('$\dot{\theta}$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+set(legend('$\dot{\theta_x}$', '$\dot{\theta_z}$'), 'Interpreter', 'latex');
 
-figure(F_1_2);
+figure(F_1_2_a);
+plot(x_1t_2, x_1t_1);
+hold on;
+plot(z_1t_2, z_1t_1);
+daspect([1 1 1])
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+xlabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+set(legend('$\mathbf{x}(t)$', '$\mathbf{z}(t)$'), 'Interpreter', 'latex');
+
+figure(F_1_2_b);
 plot(x_1t_1, x_1t_2);
 hold on;
 plot(z_1t_1, z_1t_2);
-% todo: control aspect ratio of this plot
-xlabel('x_1');
-ylabel('x_2');
+xlabel('$\theta$ [rad]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+set(legend('$\mathbf{x}(t)$', '$\mathbf{z}(t)$'), 'Interpreter', 'latex');
 
 F_2_1 = figure('Name','Individual State Evolution (starting X_0_2)','NumberTitle','off');
-F_2_2 = figure('Name','State Orbit (starting X_0_2)','NumberTitle','off');
+F_2_2_a = figure('Name','State Orbit (starting X_0_2; axis equal)','NumberTitle','off');
+F_2_2_b = figure('Name','State Orbit (starting X_0_2)','NumberTitle','off');
 
 figure(F_2_1);
+
 subplot(2,1,1);
 plot(t_2, x_2t_1);
 hold on;
 plot(t_2, z_2t_1);
-xlabel('Time (seconds)');
-ylabel('$\theta$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+set(legend('$\theta_x$', '$\theta_z$'), 'Interpreter', 'latex');
+
 subplot(212);
 plot(t_2, x_2t_2);
 hold on;
 plot(t_2, z_2t_2);
-xlabel('Time (seconds)');
-ylabel('$\dot{\theta}$', 'Interpreter','latex');
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+set(legend('$\dot{\theta_x}$', '$\dot{\theta_z}$'), 'Interpreter', 'latex');
 
-figure(F_2_2);
+figure(F_2_2_a);
+plot(x_2t_2, x_2t_1);
+hold on;
+plot(z_2t_2, z_2t_1);
+daspect([1 1 1])
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+xlabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+set(legend('$\mathbf{x}(t)$', '$\mathbf{z}(t)$'), 'Interpreter', 'latex');
+
+figure(F_2_2_b);
 plot(x_2t_1, x_2t_2);
 hold on;
 plot(z_2t_1, z_2t_2);
-% todo: control aspect ratio of this plot
-xlabel('x_1');
-ylabel('x_2');
+xlabel('$\theta$ [rad]', 'Interpreter', 'latex');
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+set(legend('$\mathbf{x}(t)$', '$\mathbf{z}(t)$'), 'Interpreter', 'latex');
 
 %% lti representations
 
