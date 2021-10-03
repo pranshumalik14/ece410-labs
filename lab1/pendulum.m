9402c8d9e9326beca656cad5016ddb8868ee7850
Pranshu Malik <pranshumalik14@gmail.com>
2021-10-03 12:21:45 -0400

    work till part 4 done

---
+++ lab1/pendulum.m
@@ -0,0 +1,15 @@
+function xdot = pendulum(t,x,parameters)
+% extract parameters
+M = parameters.M;
+g = parameters.g;
+l = parameters.l;
+
+% extract states
+x1 = x(1); x2=x(2);
+
+% set input
+u = 0;
+
+xdot = [x2;
+        -g/l*sin(x1) - 1/(M*l)*cos(x1)*u];
+
