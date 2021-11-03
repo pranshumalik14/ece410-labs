e22a8d0e1dc5c2bf1abf7682136adfbc05b5dcd8
varunsampat30 <varunsampat30@gmail.com>
2021-11-02 20:40:24 -0400

    Added sections instead of comments in lab2.m

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -1,22 +1,25 @@
-% Output 1 (Basic operations on subspaces)
-
-%  Output 1.1
-%  Output 1.2
-
-
-%  Output 3 (Linear transformations and linear equations)
-
-%  Output 3.3 
+%%  Output 1 (Basic operations on subspaces)
+V = [1 1 3; -1 1 1; 0 0 0; 1 0 1]
+W = [1 1; 0 0; 2 -2; 1 0]
 
 
-%  Output 3.4 <Pranshu>
+%% Output 1.1
+%%  Output 1.2
+
+
+%%  Output 3 (Linear transformations and linear equations)
+
+%%  Output 3.3 
+
+
+%%  Output 3.4 <Pranshu>
 
 
 
 
-%  Output 3.5 
-%  Output 3.6  
+%%  Output 3.5 
+%%  Output 3.6  
 
-%  Output 4.7 (A-invariance and Representation Theorem)
+%%  Output 4.7 (A-invariance and Representation Theorem)
 
-%  Output 5.8 (Controllability andKalman decomposition)
+%%  Output 5.8 (Controllability andKalman decomposition)
