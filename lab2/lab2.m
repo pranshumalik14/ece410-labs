ad38a087066cd312dd4d32d24a787fff39a7876b
varunsampat30 <varunsampat30@gmail.com>
2021-11-04 09:05:51 -0400

    Finished lab2 code (except part 4). Need to add comments now. Answers verified but need to cross check

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -47,7 +47,7 @@ dim_A = size(A,2)
 
 dim_img_A = rank(A)
 
-% rank-nullity states dimA = rank(A) + null(A)
+% rank-nullity theorem states dimA = rank(A) + null(A)
 % hence using the theorem
 null_A = dim_img_A - dim_A
 
@@ -57,9 +57,32 @@ null_space_dim = size(null(A),2) %need columns
 fprintf('\nThe transformation is not surjective as the output value is in R_5\nbut the rank is 3 => A cannot span all of R_5 with just 3 vectors')
 fprintf('\nSimilarly, the transformation is not injective as the rank(A) is 3.\n The row space of A cannot span all of R_4 as there are only 3 linearly independent vectors')
 %%  Output 3.6  
-b1 = [1;0;0;0;0]
-b2 = [1;1;-2;-2;-2]
+rank(A)
+% Must check rank of [A | b] 
+% If rank(A) = rank([A | b]) => b will is in the column space of A
+% Essentially, an unaffected rank will imply b is in the column space
+% But if the rank increases, then Ax = b does not have a solution
 
+b1 = [1;0;0;0;0];
+rank([A b1])
+% rank increases, so no solution
+
+b2 = [1;1;-2;-2;-2];
+rank([A b2])
+% rank unaffected here -> there exists a solution for A x = b2
+
+x = A\b2
+% Now, the nullity(A) is not 0 so to any solution for A*x = b2
+% x1 can be defined as x1 = x + k*y, where y is a vector in the nullspace
+% and k is any real number
+% null(A) indicates there is 1 vector in the nullspace of A (nullity = 1)
+
+% Consider x' = x + null(A)
+x1 = x + null(A)
+
+% Can check if Ax1 = b2
+A*x1
+% This is equal to b2. Hence, x1 is also a valid solution
 
 %%  Output 4.7 (A-invariance and Representation Theorem)
 A = [1 2 2; 1 -3 -5; -1 2 4]
@@ -68,9 +91,28 @@ V = [0 1; 1 -2; -1 1]
 % Must check Rank([AV | V]) = Rank(V)
 LHS = rank([A*V V])
 RHS = rank(V)
+% LHS == RHS => V is A-invariant
 
 W = null(V')
 P = [V W]
 
-A_hat = P^-1 * A * P
+% A_hat = inv(P) * A * P
+A_hat = P\(A*P)
+% Both yield the same result, the second option is computationally faster
+% A_hat is block upper triangular.
 %%  Output 5.8 (Controllability and Kalman decomposition)
+A = [5 -11 5; 0 -6 0; -5 5 -1];
+B = [1 -2; 0 0; 1 2];
+
+Qc = ctrb(A, B)
+% To check for controllability, must determine if rank(Qc) = n (3 in this
+% case)
+rank(Qc)
+
+V = orth(Qc)
+W = null(V')
+P = [V W]
+
+A_hat = P\(A*P)
+B_hat = P\B
+
