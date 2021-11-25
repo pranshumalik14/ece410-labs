5b56fb7ca2f04743dffdcf9bf993b7c0bd79aac8
Pranshu Malik <pranshumalik14@gmail.com>
2021-11-24 22:42:44 -0500

    init lab3 report

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,5 +1,5 @@
 %% ece410: linear control systems
-%  lab3: State Feedback Stabilization of a Cart-Pendulum Robot
+%  lab3: state feedback stabilization of a cart-pendulum robot
 %  authors: Pranshu Malik and Varun Sampat
 %  date: 24 November 2021
 
@@ -8,10 +8,12 @@ close all;
 clear all;
 
 %% parameters setup
+
 % set up initial values and DE parameters
 parameters = struct('M', 1.0731, 'm', 0.2300 , 'l', 0.3302, 'g', 9.8);
 
-%% Syms setup
+%% syms setup
+
 syms u M m l g
 syms x1 x2 x3 x4
 % x1 is y
@@ -28,7 +30,8 @@ x4_dot = (- (m*l*sin(x(3))*cos(x(3))*(x(4))^2) + (m+M)*g*sin(x(3)) + u*cos(x(3))
 x_dot = [x1_dot; x2_dot; x3_dot; x4_dot]
 
 % y = x1;
-%% Linearize at (0,0,0,0)
+%% linearize at (0,0,0,0)
+
 x_star = [0; 0; 0; 0];
 u_star = 0;
 
@@ -41,16 +44,19 @@ A = subs(symA, {x1, x2, x3, x4, u}, {x_star(1), x_star(2), x_star(3), x_star(4),
 B = subs(symB, {x1, x2, x3, x4, u}, {x_star(1), x_star(2), x_star(3), x_star(4), u})
 
 %% parameters
+
 Asub = subs(A, {g, m, M, l}, {parameters.g, parameters.m, parameters.M, parameters.l})
 Bsub = subs(B, {g, m, M, l}, {parameters.g, parameters.m, parameters.M, parameters.l})
 
 
-%% Test for controllability
+%% test for controllability
+
 Qc = ctrb(Asub, Bsub)
 
 % Rank of Qc tells us if the system is controllable
 disp(rank(Qc))
 
-%% Generate K1
+%% generate K1
+
 p = [-1 -2 -3 -4]
 K1 = place(Asub, Bsub, p)
