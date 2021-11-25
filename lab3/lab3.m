c16e3169d5061662b67d391a78d2b988b047ef20
Pranshu Malik <pranshumalik14@gmail.com>
2021-11-25 17:41:47 -0500

    adding changes

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -12,46 +12,39 @@ clear all;
 % set up initial values and DE parameters
 parameters = struct('M', 1.0731, 'm', 0.2300 , 'l', 0.3302, 'g', 9.8);
 
-%% syms setup
+%% symbolic linearization of the control system
 
 syms u M m l g
 syms x1 x2 x3 x4
-% x1 is y
-% x2 is y_dot
-% x3 is theta
-% x4 is theta_dot
+
+% x1 = y; horizontal position
+% x2 = y_dot
+% x3 = theta; angle from the vertically upwards axis
+% x4 = theta_dot
 x    = [x1; x2; x3; x4];
 
 x1_dot = x(2);
-x2_dot = (- (m*l*sin(x(3))*(x(4))^2) + (m*g*sin(x(3))*cos(x(3))) + u)/(M + m*sin(x(3))^2);
+x2_dot = (-(m*l*sin(x(3))*(x(4))^2) + (m*g*sin(x(3))*cos(x(3))) + u) / (M + m*sin(x(3))^2);
 x3_dot = x(4);
-x4_dot = (- (m*l*sin(x(3))*cos(x(3))*(x(4))^2) + (m+M)*g*sin(x(3)) + u*cos(x(3)) )/ ( l * (M + m*sin(x(3))^2));
+x4_dot = (-(m*l*sin(x(3))*cos(x(3))*(x(4))^2) + (m+M)*g*sin(x(3)) + u*cos(x(3))) / (l * (M + m*sin(x(3))^2));
 
-x_dot = [x1_dot; x2_dot; x3_dot; x4_dot]
+x_dot = [x1_dot; x2_dot; x3_dot; x4_dot];
 
-% y = x1;
-%% linearize at (0,0,0,0)
-
-x_star = [0; 0; 0; 0];
-u_star = 0;
-% Actually can't assume u_star is 0
-% Taking the jacobian results in a system indp of u
+% linearization equilibrium (for all comparisons going forward)
+xstar = [0; 0; 0; 0];
 
 symA = jacobian(x_dot,x);
 symB = jacobian(x_dot,u);
 
 
-% substitute the symbolic variables with the equilibrium points 
-A = subs(symA, {x1, x2, x3, x4, u}, {x_star(1), x_star(2), x_star(3), x_star(4), u})
-B = subs(symB, {x1, x2, x3, x4, u}, {x_star(1), x_star(2), x_star(3), x_star(4), u})
+% substitute the symbolic variables with the equilibrium points
+A = subs(symA, {x1, x2, x3, x4, u}, {xstar(1), xstar(2), xstar(3), xstar(4), u});
+B = subs(symB, {x1, x2, x3, x4, u}, {xstar(1), xstar(2), xstar(3), xstar(4), u});
 
-%% parameters
+%% converting to numeric state-space representation
 
-Asub = subs(A, {g, m, M, l}, {parameters.g, parameters.m, parameters.M, parameters.l})
-Bsub = subs(B, {g, m, M, l}, {parameters.g, parameters.m, parameters.M, parameters.l})
-
-Asub = cast(Asub, 'double')
-Bsub = cast(Bsub, 'double')
+numA = double(subs(A, {g, m, M, l}, {parameters.g, parameters.m, parameters.M, parameters.l}));
+numB = double(subs(B, {g, m, M, l}, {parameters.g, parameters.m, parameters.M, parameters.l}));
 
 %% test for controllability
 
