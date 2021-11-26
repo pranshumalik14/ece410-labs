a7d46cee12ec75b986821ff50c3685f4e3fbcb4a
Pranshu Malik <pranshumalik14@gmail.com>
2021-11-26 17:55:46 -0500

    adding plotting function and code corrections

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,7 +1,7 @@
 %% ece410: linear control systems
 %  lab3: state feedback stabilization of a cart-pendulum robot
 %  authors: Pranshu Malik and Varun Sampat
-%  date: 24 November 2021
+%  date: 26 November 2021
 
 clc;
 close all;
@@ -57,7 +57,7 @@ assert(rank(Qc) == min(size(Qc)));
 p1 = [-1 -2 -3 -4];
 K1 = -1*place(numA, numB, p1) % use u = Kx instead of u = -Kx
 
-% create cls (closed loop system) for K1
+% create linearized cls (closed loop system) for K1
 A_cls1 = numA + numB*K1;
 cls1   = @(t, x) A_cls1*x;
 
@@ -65,11 +65,11 @@ cls1   = @(t, x) A_cls1*x;
 x0 = [-0.5; 0; -pi/4; 0];
 u0 = K1*x0;
 
-% setup Tspan
+% setup integration error tol and Tspan
 options    = odeset('RelTol', 1e-7, 'AbsTol', 1e-7);
-Tspan      = linspace(0,10,1e3);
+Tspan      = linspace(0, 10, 1e3);
 
-% [t1, X1] =  ode45(@inverted_pendulum, Tspan, [x0; u0], options, parameters, K1);
+% [t1, X1] =  ode45(@inverted_pendulum, Tspan, [x0; u0], options, parameters, K1); % non-linear system sim
 [t1, X1] = ode45(cls1, Tspan, x0);
 
 % extract state evolutions for K1
@@ -77,20 +77,20 @@ x1_t1 = X1(:,1);
 x2_t1 = X1(:,2);
 x3_t1 = X1(:,3);
 x4_t1 = X1(:,4);
-% u_t1  = X1(:,5);
+% u_t1  = X1(:,5); % from sim for non-linear system
 
 % extract control input, u, for K1
-u_t1 = X1*K1'; % = (K1*X1')'
+u_t1 = X1*(K1'); % = (K1*X1')'
 
 % generate K2
 p2 = [-1 -2 -3 -20];
 K2 = -1*place(numA, numB, p2) % use u = Kx instead of u = -Kx
 
-% create cls system for K2
+% create linearized cls system for K2
 A_cls2 = numA + numB*K2;
 cls2   = @(t, x) A_cls2*x;
 
-%[t2, X2] = ode45(@inverted_pendulum, Tspan, [x0; u0], options, parameters, K2);
+%[t2, X2] = ode45(@inverted_pendulum, Tspan, [x0; u0], options, parameters, K2); % non-linear system sim
 [t2, X2] = ode45(cls2, Tspan, x0);
 
 % extract state evolutions for K2
@@ -98,124 +98,179 @@ x1_t2 = X2(:,1);
 x2_t2 = X2(:,2);
 x3_t2 = X2(:,3);
 x4_t2 = X2(:,4);
-% u_t2 = X2(:,5);
+% u_t2 = X2(:,5); % from sim for non-linear system
 
 % extract control input, u, for K2
-u_t2 = X2*K2'; % = (K2*X2')'
+u_t2 = X2*(K2'); % = (K2*X2')'
 
-figure
+% plot state and control evolution over time for both gains K1 and K2
+fig_X_pp = figure('Name', 'State and Control Evolution with Pole Placement (starting x0)', 'NumberTitle', 'off');
+
+figure(fig_X_pp);
+subplot(5,1,1);
 plot(t1, x1_t1)
-hold on
+hold on;
 plot(t2, x1_t2)
-
-figure
+ylabel('$y$ [m]', 'Interpreter', 'latex');
+set(legend('$y_{K_1}$', '$y_{K_2}$'), 'Interpreter', 'latex');
+subplot(5,1,2);
+plot(t1, x2_t1)
+hold on;
+plot(t2, x2_t2)
+ylabel('$\dot{y}$ [m/s]', 'Interpreter', 'latex');
+set(legend('$\dot{y}_{K_1}$', '$\dot{y}_{K_2}$'), 'Interpreter', 'latex');
+subplot(5,1,3);
+plot(t1, x3_t1)
+hold on;
+plot(t2, x3_t2)
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex');
+set(legend('$\theta_{K_1}$', '$\theta_{K_2}$'), 'Interpreter', 'latex');
+subplot(5,1,4);
+plot(t1, x4_t1)
+hold on;
+plot(t2, x4_t2)
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex');
+set(legend('$\dot{\theta}_{K_1}$', '$\dot{\theta}_{K_2}$'), 'Interpreter', 'latex');
+subplot(5,1,5);
 plot(t1, u_t1)
-hold on
+hold on;
 plot(t2, u_t2)
+xlabel('Time [s]', 'Interpreter', 'latex');
+ylabel('$u$ [N]', 'Interpreter', 'latex');
+set(legend('$u_{K_1}$', '$u_{K_2}$'), 'Interpreter', 'latex');
 
 %% linear quadratic optimal control for stabilization at equilibrium
+% note: now to compute controlled state response (for linearized system, we 
+% will use the function plot_linearized_cls_response()
+% also note: all simulations are done for the same x0 = (-0.5, 0, -pi/4, 0)
 
 syms q1 q2
 symQ = [q1 0 0 0; 0 0 0 0; 0 0 q2 0; 0 0 0 0];
 
-% impact of changing q1
-R = 0.5;
+% impact of changing q1 on controlled state time response
+R      = 0.5;
 num_q2 = 5;
 
-% exp 1
+% K for q1 = 0.1
 num_q1_1 = 0.1;
+numQ_1   = double(subs(symQ, {q1, q2}, {num_q1_1, num_q2}));
+K_q1_1   = -1*lqr(numA, numB, numQ_1, R);
 
-Q_num_1 = double(subs(symQ, {q1, q2}, {num_q1_1, num_q2}));
-K1_1 = -1*lqr(numA, numB, Q_num_1, R)
-
-% exp 2
+% K for q1 = 0.005
 num_q1_2 = 0.005;
-Q_num_2 = double(subs(symQ, {q1, q2}, {num_q1_2, num_q2}));
-K1_2 = -1*lqr(numA, numB, Q_num_2, R);
+numQ_2   = double(subs(symQ, {q1, q2}, {num_q1_2, num_q2}));
+K_q1_2   = -1*lqr(numA, numB, numQ_2, R);
 
-% impact of changing q2
+% time control responses for controllers u = K_q1_1*x and u = K_q1_2*x
+fig_q1_1 = figure('Name', 'State and Control Evolution for K_q1_1 (starting x0)', 'NumberTitle', 'off');
+fig_q1_2 = figure('Name', 'State and Control Evolution for K_q1_2 (starting x0)', 'NumberTitle', 'off');
+cls_q1_1 = @(t, x) (numA + numB*K_q1_1)*x;
+cls_q1_2 = @(t, x) (numA + numB*K_q1_2)*x;
+
+[~, X_q1_1] = plot_linearized_cls_response(fig_q1_1, cls_q1_1, K_q1_1, Tspan, x0);
+[~, X_q1_2] = plot_linearized_cls_response(fig_q1_2, cls_q1_2, K_q1_2, Tspan, x0);
+
+% impact of changing q2 on controlled state time response
+R      = 0.5;
 num_q1 = 0.05;
-R = 0.5;
 
-% exp 1
+% K for q2 = 1
 num_q2_1 = 1;
-Q_num_1 = double(subs(symQ, {q1, q2}, {num_q1_1, num_q2_1}));
-K2_1 = -1*lqr(numA, numB, Q_num_1, R);
+numQ_1   = double(subs(symQ, {q1, q2}, {num_q1, num_q2_1}));
+K_q2_1   = -1*lqr(numA, numB, numQ_1, R);
 
-% exp 2
+% K for q2 = 2000
 num_q2_2 = 2000;
-Q_num_2 = double(subs(symQ, {q1, q2}, {num_q1_1, num_q2_2}));
-K2_2 = -1*lqr(numA, numB, Q_num_2, R);
+numQ_2   = double(subs(symQ, {q1, q2}, {num_q1, num_q2_2}));
+K_q2_2   = -1*lqr(numA, numB, numQ_2, R);
+
+% time control responses for controllers u = K_q2_1*x and u = K_q2_2*x
+fig_q2_1 = figure('Name', 'State and Control Evolution for K_q2_1 (starting x0)', 'NumberTitle', 'off');
+fig_q2_2 = figure('Name', 'State and Control Evolution for K_q2_2 (starting x0)', 'NumberTitle', 'off');
+cls_q2_1 = @(t, x) (numA + numB*K_q2_1)*x;
+cls_q2_2 = @(t, x) (numA + numB*K_q2_2)*x;
+
+[~, X_q2_1] = plot_linearized_cls_response(fig_q2_1, cls_q2_1, K_q2_1, Tspan, x0);
+[~, X_q2_2] = plot_linearized_cls_response(fig_q2_2, cls_q2_2, K_q2_2, Tspan, x0);
 
 % impact of changing R
-
 num_q1 = 0.05;
 num_q2 = 5;
-Q_num = double(subs(symQ, {q1, q2}, {num_q1_1, num_q2_2}));
+numQ   = double(subs(symQ, {q1, q2}, {num_q1, num_q2}));
 
-% exp 1
-num_R_1 = 0.0005;
-K3_1 = -1*lqr(numA, numB, Q_num, num_R_1);
+% K for R = 0.005
+numR_1 = 0.005;
+K_R_1   = -1*lqr(numA, numB, numQ, numR_1);
 
-% exp 2
-num_R_2 = 10;
-K3_2 = -1*lqr(numA, numB, Q_num, num_R_1);
+% K for R = 10
+numR_2 = 10;
+K_R_2  = -1*lqr(numA, numB, numQ, numR_2);
 
-% need a function that takes in K, Tspan, A, B, x0, and outputs generated
-% plots
+% time control responses for controllers u = K_R_1*x and u = K_R_2*x
+fig_R_1 = figure('Name', 'State and Control Evolution for K_R_1 (starting x0)', 'NumberTitle', 'off');
+fig_R_2 = figure('Name', 'State and Control Evolution for K_R_2 (starting x0)', 'NumberTitle', 'off');
+cls_R_1 = @(t, x) (numA + numB*K_R_1)*x;
+cls_R_2 = @(t, x) (numA + numB*K_R_2)*x;
+
+[~, X_R_1] = plot_linearized_cls_response(fig_R_1, cls_R_1, K_R_1, Tspan, x0);
+[~, X_R_2] = plot_linearized_cls_response(fig_R_2, cls_R_2, K_R_2, Tspan, x0);
 
 %% nonlinear comparison of lqr controller performance
 
 x0 = [-1; 0; -pi/4; 0]; % new initial state
 
 % get nonlinear evolution of X given our control law, u = Kx
-[t, Xref] = ode45(@inverted_pendulum, Tspan, [x0; u0], options, parameters, K3_1);
+[t, Xu_nl] = ode45(@inverted_pendulum, Tspan, [x0; u0], options, parameters, K_R_1);
+X_nl = Xu_nl(:, 1:4);
+u_nl = Xu_nl(:, 5);
 
-x_non_lin = Xref(:, 1:4);
-u_non_lin = Xref(:, 5);
-
-% create cls system for K3_1
-A_cls = numA + numB*K3_1;
+% get linearized cls conntrolled state time response for K_R_1
+A_cls = numA + numB*K_R_1;
 cls   = @(t, x) A_cls*x;
 
-[t2, Xlin] = ode45(cls, Tspan, x0);
-u_lin = Xlin*(K3_1');
+[t2, X_l] = ode45(cls, Tspan, x0);
+u_l = X_l*(K3_1');
 
 figure
-plot(t, Xlin(:,1) , t, x_non_lin(:,1))
+plot(t, X_l(:,1) , t, X_nl(:,1))
 
 %%
 
 x0_bar = [-175.7; 0; -pi/4; 0];
-Tspan      = linspace(0,0.5,1e3);
+Tspan  = linspace(0,0.5,1e3);
 
 % get nonlinear evolution of X given our control law, u = Kx
-[t, Xref] = ode45(@inverted_pendulum, Tspan, [x0_bar; u0], options, parameters, K3_1);
+[t, X_nl] = ode45(@inverted_pendulum, Tspan, [x0_bar; u0], options, parameters, K3_1);
 
-x_non_lin = Xref(:, 1:4);
-u_non_lin = Xref(:, 5);
+X_nl = X_nl(:, 1:4);
+u_nl = X_nl(:, 5);
 
-[t, Xlin] = ode45(cls, Tspan, x0);
-x_lin = Xlin(:, 1:4);
-u_lin = Xlin*(K3_1');
+[t, X_l] = ode45(cls, Tspan, x0);
+x_lin = X_l(:, 1:4);
+u_l = X_l*(K3_1');
 
 figure
-plot(t, x_non_lin(:,1))
+plot(t, X_nl(:,1))
 hold on
 plot(t, x_lin(:,1))
 figure
-plot(t, x_non_lin(:,2))
+plot(t, X_nl(:,2))
 hold on
 plot(t, x_lin(:,2))
 figure
-plot(t, x_non_lin(:,3))
+plot(t, X_nl(:,3))
 hold on
 plot(t, x_lin(:,3))
 figure
-plot(t, x_non_lin(:,4))
+plot(t, X_nl(:,4))
 hold on
 plot(t, x_lin(:,4))
 figure
-plot(t, u_non_lin)
+plot(t, u_nl)
 hold on
-plot(t, u_lin)
+plot(t, u_l)
+
+%% autoexport figures to (pdf) files
+%  note: uncomment to save again
+
+% savefig(f_1_1, './figs/section3_x0_1_state_evolution')
